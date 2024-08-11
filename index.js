8dca0a9a887209c8ab49ea4baacc151a0462ca56
RezaRifaldi076 <lelmanzj@gmail.com>
2024-08-12 04:22:00 +0800

    tes 3

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const router = express.Router();
 const { google } = require("googleapis");
 const date = require('date-and-time');
+const cors = require('cors');
+const corsConfig = {
+	origin : "*",
+	credential : true,
+	method : ["GET", "POST", "PUT", "DELETE"],
+};
 const home = require("./routes/home");
 
 // Middlewares
@@ -13,8 +19,10 @@ const home = require("./routes/home");
 // Routes
 //app.use("/home", home);
 
+app.options("", cors(corsConfig));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsConfig));
 
 app.get("/", (req, res) => {
 res.render("index");
--- routes/home.js
+++ routes/home.js
@@ -13,11 +13,4 @@ router.get("/", async (req, res, next) => {
 });
 */
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-res.render("index");
-});
-
 module.exports = router;
