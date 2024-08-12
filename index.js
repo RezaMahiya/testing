da0f4b3287b571b489fa829ce8840cb8cd6d87bd
RezaRifaldi076 <lelmanzj@gmail.com>
2024-08-12 13:45:46 +0800

    tes 11

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const router = express.Router();
 const { google } = require("googleapis");
 const date = require('date-and-time');
+
+require('dotenv').config();
 //const cors = require('cors');
 /*const corsConfig = {
 	origin : "*",
@@ -11,25 +13,60 @@ const date = require('date-and-time');
 	method : ["GET", "POST", "PUT", "DELETE"],
 };
 */
-const home = require("./routes/home");
-const fs = require('fs');
-app.set("view engine", "ejs");
+//const home = require("./routes/home");
 app.use(express.urlencoded({ extended: true }));
 
 // Middlewares
-//const app = express();
 app.use(express.json());
-
 // Routes
-app.use("/home", home);
-/*
-app.options("", cors(corsConfig));
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsConfig));
-*/
+//app.use("/home", home);
+
 app.get("/", (req, res) => {
 res.sendFile('./views/index.html',{root: __dirname});
+});
+
+app.post("/", async (req, res) => {
+
+  const {tester} = req.body;
+
+  const now = new Date();
+  let tanggal = date.format(now, 'DD/MM/YYYY');
+  let waktu = date.format(now,'HH:mm:ss');
+     
+  const credentialsEnv = process.env.GOOGLE_CREDENTIALS;
+  const credentials = JSON.parse(credentialsEnv);
+  
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: credentials.client_email,
+      private_key: credentials.private_key.replace(/\\n/g, '\n'), // Handle newlines
+      },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1Fw3dMbgyuInftlDW66lU7_FH76aIlG3m5RVxf2QQEGA";
+
+  
+  // tambahdata about spreadsheet
+  const tambahdata = await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range : "Sheet1!A:C",
+    valueInputOption : "USER_ENTERED",
+    resource : {
+        values : [
+            [tester, tanggal, waktu],
+        ]
+    },
+  });
+  
+  res.sendFile('./views/index.html',{root: __dirname});
 });
 
 // connection
