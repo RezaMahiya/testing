c6ba3d38a45dc7d744ba13f34bd64985406da38c
RezaRifaldi076 <lelmanzj@gmail.com>
2024-08-12 11:14:05 +0800

    tes 8

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,6 @@ const app = express();
 const { google } = require("googleapis");
 const date = require('date-and-time');
 const fs = require('fs');
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 
 router.get("/", async (req, res) => {
   res.render('index');
